a0f08e38f66c6a409deef8dcdef50901b65c5941
kurowrath <zagreus.utility@gmail.com>
2023-03-16 14:07:03 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description : a C program that prints exactly and that piece of art is useful
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+}
