59707961cdd0565c6ee53f886c98826c2247e230
kurowrath <zagreus.utility@gmail.com>
2023-03-16 10:29:56 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * return: 0 (success)
+ * return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
