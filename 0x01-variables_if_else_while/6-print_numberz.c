a6a34bee875eda9e443c8e00ba48ed67c7449a52
kurowrath <zagreus.utility@gmail.com>
2023-03-18 00:11:13 +0100

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: prints all single digits from 0 to 10 without using char var no printf 
+ * Description: prints all single digits from 0 to 10 w/o using printf
  *
  * Return: Alays 0 (Success)
 */
