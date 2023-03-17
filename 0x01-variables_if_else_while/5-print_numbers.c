7f52d7aa0cac80262f72b486265236acb8ea1811
kurowrath <zagreus.utility@gmail.com>
2023-03-17 23:55:51 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: printing single digits 0-10
+ * 
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int dj = 0;
+
+	while (dj <= 9)
+	{
+		printf("%d",dj);
+		dj++;
+	}
+	putchar('\n');
+return (0);
+}
