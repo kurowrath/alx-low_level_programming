61ddc4433c8545bad55e7c60a23219f1a7453213
kurowrath <zagreus.utility@gmail.com>
2023-03-17 23:24:56 +0100

    . When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,12 +15,12 @@ int main(void)
 
         while (c <= 'z')
         {
-                if (c != 'e' && c != 'q')
+                if (c !='e' && c !='q')
                 {
                         putchar(c);
                 }
                 c++;
         }
-        putchar('\n');
+        pytchar('\n');
 return (0);
 }
