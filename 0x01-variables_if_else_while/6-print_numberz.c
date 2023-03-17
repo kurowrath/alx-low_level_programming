fedf4c209fa13d45f566bc1e7f2093c02846c5f0
kurowrath <zagreus.utility@gmail.com>
2023-03-18 00:06:00 +0100

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digits from 0 to 10 without using char var no printf 
+ *
+ * Return: Alays 0 (Success)
+*/
+
+int main(void)
+{
+	int dj = 0;
+
+	while (dj <= 9)
+	{
+		putchar((char)dj);
+		dj++;
+	}
+	putchar('\n')
+return (0);
+}
