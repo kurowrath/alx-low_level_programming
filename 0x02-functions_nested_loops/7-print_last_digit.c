29748281b08c3c26515184b6a6861e3e6d27d5bd
kurowrath <zagreus.utility@gmail.com>
2023-03-21 23:50:16 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *
+*/
+
+
+
+int print_last_digit(int)
+{
+	int n ;
+	int result;
+
+	result = n % 10;
+	_putchar(result + 0);
+return (0);
+}
