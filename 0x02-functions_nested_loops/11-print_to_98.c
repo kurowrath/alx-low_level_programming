87ef48477e9d139a7397f21fb239b0d94852ff71
kurowrath <zagreus.utility@gmail.com>
2023-03-24 21:45:09 +0000

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - prints abs of a number
+ *
+ * @x: first number
+ * @y: second  number
+ *
+ * Return: Returns the sum of 2 numbers
+*/
+
+int add(int x, int y)
+{
+	int r;
+
+	r = x + y;
+	return (r);
+}
