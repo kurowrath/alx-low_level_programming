da1442d12e91e21b5beb72ad5e68b2f9af3370a3
kurowrath <zagreus.utility@gmail.com>
2023-03-24 23:02:50 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+/**
+ * print_numbers - print from 0to 9
+ *
+ * Return: Always 0(Success)
+*/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <=9) 
+	{
+		_putchar(i + 48);
+		i++;
+	}
+	_putchar('\n');
+}
