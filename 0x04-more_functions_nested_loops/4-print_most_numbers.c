e39f48d62297fd43ab57bbb467f7a19622bba559
kurowrath <zagreus.utility@gmail.com>
2023-03-24 23:34:37 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,19 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while(i >= 9)
+	for (i = 0; i <= 9 ; i++)
 	{
 		if (i == 2 || i == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar( i + 48);
-		}
-		i++;
+		continue;
+		_putchar(i + 48);
 	}
 	_putchar('\n');
 }
