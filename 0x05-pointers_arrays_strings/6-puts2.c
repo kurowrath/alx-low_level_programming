4c683413809957972141ef37673502c8977e1c77
kurowrath <zagreus.utility@gmail.com>
2023-03-27 22:10:28 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * 	starting with the first character, followed by a new line.
+ *
+ * @str: input parameter
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+		for (int i = 0 ; i < sizeof(*str)/sizeof(*str[0]); i++)
+	{
+		printf("%lf\n",*str[i]);
+	}
+}
+
