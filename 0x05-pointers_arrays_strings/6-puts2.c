e483ab4e9177b0a48c5da6cd37fd491d139d82b8
kurowrath <zagreus.utility@gmail.com>
2023-03-27 22:26:40 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - prints every other character of a string,
- * 	starting with the first character, followed by a new line. 
+ *	starting with the first character, followed by a new line. 
  *
  * @str: input parameter
  *
