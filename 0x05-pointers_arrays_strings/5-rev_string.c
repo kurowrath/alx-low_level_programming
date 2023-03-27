d40d5037eb09c6b4f79db26a6f3515840da69be5
kurowrath <zagreus.utility@gmail.com>
2023-03-27 22:02:20 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,8 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 	/*fidn a string lenght without a null character*/
-	for (l = 0; s[l] != '\0'; ++1);
+	for (l = 0; s[l] != '\0'; ++1)
+		;
 
 	/*swap the string by looping around the half string*/
 	for (i = 0; i < 1 / 2; i++)
