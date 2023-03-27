b95ffe152fe6b47366381e7c4fa642d2247f3719
kurowrath <zagreus.utility@gmail.com>
2023-03-27 22:14:30 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,9 +12,13 @@
 void puts2(char *str)
 {
 	int i;
-		for (i = 0 ; i < sizeof(*str)/sizeof(*str[0]); i++)
+
+	for (i = 0 ; str[i] != '\0' ; ++i)
 	{
-		printf("%lf\n",*str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
+	_putchar('\n');
+	
 }
 
