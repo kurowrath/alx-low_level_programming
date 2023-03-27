81c006f7e0cefbac289438e9ec4aa5860c0fa18b
kurowrath <zagreus.utility@gmail.com>
2023-03-27 21:31:55 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void swap_int - swaps the values of two integers.
+ *
+ * @a: input integer
+ * @b: input integer
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
