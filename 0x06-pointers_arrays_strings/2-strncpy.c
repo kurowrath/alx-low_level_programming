9e0c7c3e8db23e1bcb12b08dd0bc138f7de277f3
kurowrath <zagreus.utility@gmail.com>
2023-03-29 22:34:03 +0000

    4-rev_array.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,12 @@
-#include "main.h"
-
 /**
- * _strncpy - a function that copies a string 
+ * _strncpy - A function that copies a string.
  *
  * @dest: pointer to destination input buffer
  * @src: pointer to source input buffer
- * @: bytes of @src
+ * @n: bytes of @src
  *
  * Return: @dest
- */
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
