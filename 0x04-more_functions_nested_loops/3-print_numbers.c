b395de9885aefc1fccd13992cf918367fd1202ee
kurowrath <zagreus.utility@gmail.com>
2023-04-02 14:13:43 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include"main.h"
 
 /**
- *print_numbers - print from 0 - 9 
- *		only using _putchar 2 times
- *Return: Always 0 (Success)
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
 */
 
 void print_numbers(void)
 {
 	int num = 0;
 
-	do{
+	do {
 		_putchar(num + 48);
 		num++;
 	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
+
