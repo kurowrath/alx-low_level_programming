0170a57d53ff0d2612d1bf38ee2e8f8b5bd795a0
kurowrath <zagreus.utility@gmail.com>
2023-04-03 17:31:35 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: buffer copy destination
+ * @src: the copied 
+ * @n: n bytes of @src
+ *
+ * Return: Always 0 (Success)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
